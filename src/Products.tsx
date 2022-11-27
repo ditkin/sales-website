5b3c8c7ab70aa3985647ac1f8de5aecc4ac97001
David Itkin <ditkin@hubspot.com>
2022-11-26 19:27:23 -0500

    Use Oswald font; add price

--- src/Products.tsx
+++ src/Products.tsx
@@ -6,19 +6,22 @@ const productData = [
     image: closers,
     buyNowUrl:
       'https://app.hubspot.com/payments/04jIf8ie?referrer=PAYMENT_LINK',
+    price: '$35.00',
   },
   {
     image: gottem,
     buyNowUrl:
       'https://app.hubspot.com/payments/Q-zVMBRcQs-L?referrer=PAYMENT_LINK',
+    price: '$50.00',
   },
 ];
 
 interface ProductRowProps {
   image: string;
   buyNowUrl: string;
+  price: string;
 }
-function ProductRow({ image, buyNowUrl }: ProductRowProps) {
+function ProductRow({ image, buyNowUrl, price }: ProductRowProps) {
   function onClick() {
     window.open(buyNowUrl, '_blank');
   }
@@ -36,7 +39,10 @@ function ProductRow({ image, buyNowUrl }: ProductRowProps) {
         <img src={image} />
       </div>
       <div style={{ width: '42%', textAlign: 'center' }}>
-        <button onClick={onClick}>BUY NOW</button>
+        <button onClick={onClick} style={{ fontFamily: 'Oswald, sans-serif' }}>
+          <u>BUY NOW</u> <br />
+          {price}
+        </button>
       </div>
     </div>
   );
@@ -46,9 +52,7 @@ export default function Products() {
   return (
     <div>
       {productData.map(product => {
-        return (
-          <ProductRow image={product.image} buyNowUrl={product.buyNowUrl} />
-        );
+        return <ProductRow {...product} />;
       })}
     </div>
   );
